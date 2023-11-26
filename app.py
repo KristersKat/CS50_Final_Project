beca7d7d199894855f7bf7b90c7532dd26c38c88
Kristers Katkevics-Cirkovs <kristerskat@gmail.com>
2023-11-26 03:12:00 +0200

    Added user info saving function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
@@ -8,22 +8,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Defined the user that is used in the table (If the table is modified, this has to be modified as well)
-class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    item = db.Column(db.String(120), nullable=False)
-
 @app.route('/')
 def index():
-    db.session.execute(text("INSERT INTO Users (name, item) VALUES ('Ojars', 'dators')"))
-    db.session.commit()
+    #db.session.execute(text("INSERT INTO Users (name, item) VALUES ('Ojars', 'dators')"))
+    #db.session.commit()
 
     result = db.session.execute(text("SELECT * FROM Users"))
     user_list = result.fetchall()
     
     print(user_list)
-    return render_template("karte.html")
+    return render_template("testLocation.html")
+
+# Adds new user to database
+@app.route('/newuser', methods = ['POST'])
+def newuser():
+    name = request.get_data(as_text=True)
+
+    db.session.execute(text("INSERT INTO Users (name) VALUES (:name)"), {"name": name})
+    id = db.session.execute(text("SELECT LAST_INSERT_ID()")).scalar()
+    items = db.session.execute(text("SELECT item FROM Users WHERE user_id = :id"), {"id": id}).scalar()
+    db.session.commit()
+
+    return jsonify({"id": id, "items": items})
+
+# Returns info about items in users inventory
+@app.route('/items', methods = ['POST'])
+def items():
+    id = request.get_data(as_text=True)
+    items = db.session.execute(text("SELECT item FROM Users WHERE user_id = :id"), {"id": id}).scalar()
+    return jsonify({"items": items})
 
 # Checks if it is run directly and then runs it in debug mode
 if __name__ == '__main__':
---
+++ static/userInfo.js
@@ -0,0 +1,55 @@
+async function getUserInfo()
+{
+    let id, name, items;
+
+    // Checks if there is information about the user
+    if (!localStorage.getItem("user_id") || !localStorage.getItem("name"))
+    {
+        let name;
+        // Check if name isn't empty
+        do
+        {
+            name = prompt("Enter your name"); // Edit to change the way the script prompts for a name
+        } while (name === null || name.trim() === '')
+
+        // Fetches the new user id and items (sends the name of user)
+        const response = await fetch("http://127.0.0.1:5000/newuser", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+            body: name,
+        })
+        user = await response.json();
+        id = user.id + ''; // Turns id to string so it is consistent for both a new and returning user
+        items = user.items;
+
+        // Sets info about user in local storage
+        localStorage.setItem("user_id", id);
+        localStorage.setItem("name", name);
+        localStorage.setItem("items", items);
+        return {id, name, items};
+    }
+
+    // If ther is information about user, updates the items
+    id = localStorage.getItem("user_id");
+    name = localStorage.getItem("name");
+
+    const response = await fetch("http://127.0.0.1:5000/items", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+        body: id,
+    })
+    items = await response.json();
+    items = items.items;
+
+    localStorage.setItem("items", items);
+    return {id, name, items};
+}
+
+(async () => {
+    const user = await getUserInfo();
+    console.log(user);
+})();
